76f04c7968166d90485a31003776d94f17461d91
Rachel Hovell <r.hovell@live.com>
2018-08-16 19:34:50 +0100

    Changed rendering into router based pages
    Removed bulk of HTML in rendering, added route links to pages

---
+++ src/Books.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Shelves extends Component {
+
+  render(){
+    return (
+      <div>Hello</div>
+    )
+  }
+}
