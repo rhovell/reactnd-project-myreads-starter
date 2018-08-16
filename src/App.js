14b014cf3268d230ff5c24d6da69553d6eff9c16
Rachel Hovell <r.hovell@live.com>
2018-08-16 19:55:05 +0100

    Separated app functions into search and shelves

--- src/App.js
+++ src/App.js
@@ -4,73 +4,26 @@ import { Route } from 'react-router-dom'
 import './App.css'
 import Shelves from './Books'
 import { Link } from 'react-router-dom'
+import SearchForm from './Search'
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    Books: [{
+      want: [],
+      current: [],
+      read: []
+    }]
   }
 
   render() {
     return (
       <div className="app">
         <Route path="/search" render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-            <Link className="close-search" to="/">Close Search</Link>
-                <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-                </div>
-            </div>
-            <div className="search-books-results">
-            <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchForm />
         )}/>
 
         <Route exact path="/" render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-            <Link className="add-a-book" to="/search">Add a book</Link>
-            </div>
-          </div>
+          <Shelves />
           )}/>
       </div>
     )}
