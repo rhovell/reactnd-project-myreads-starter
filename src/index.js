e77c80fb65260b9f31ee2229e948a48026d55717
Rachel Hovell <r.hovell@live.com>
2018-12-09 16:12:10 +0000

    added build project files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import BooksApp from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter><App /></BrowserRouter>,
+  <BrowserRouter><BooksApp /></BrowserRouter>,
   document.getElementById('root')
 );
